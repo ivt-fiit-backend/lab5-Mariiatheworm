eb5a6d00fc6bb05c26d4fbfc2db247c76cfd4b92
mariiatheworm <map03032003@gmail.comm>
2026-02-27 19:54:10 +0900

    lab5 finished

--- app.py
+++ app.py
@@ -1,33 +1,38 @@
 import json
 from flask import Flask, abort, jsonify, request
-from flask_restx import Api  # type: ignore
+from flask_restx import Api, Resource
 
 PAGE_SIZE = 25
 
 app = Flask(__name__)
 api = Api(app)
 
-with open('awards.json', encoding='utf-8') as f:
+with open("awards.json", encoding="utf-8") as f:
     awards = json.load(f)
 
 # TODO: Добавить код для чтения лауреатов из файла
+
+with open("laureats.json", encoding="utf-8") as f:
+    laureats = json.load(f)
 
 
 @app.route("/api/v1/awards/")
 def awards_list():
     try:
-        p = int(request.args.get('p', 0))
+        p = int(request.args.get("p", 0))
         if p < 0:
             raise ValueError
     except ValueError:
         return abort(400)
-    page = awards[p * 50:(p + 1)*50]
-    return jsonify({
-        'page': p,
-        'count_on_page': PAGE_SIZE,
-        'total': len(awards),
-        'items': page,
-    })
+    page = awards[p * 50: (p + 1) * 50]
+    return jsonify(
+        {
+            "page": p,
+            "count_on_page": PAGE_SIZE,
+            "total": len(awards),
+            "items": page,
+        }
+    )
 
 
 @app.route("/api/v1/award/<int:pk>/")
@@ -41,4 +46,40 @@ def award_object(pk):
 # TODO: Добавить код для получения списка лауреатов
 
 
+ns = api.namespace("v2", description="version2")
+
+
+@ns.route("/laureats/")
+class Laureats(Resource):
+    def get(self):
+        try:
+            p = int(request.args.get("p", 0))
+            if p < 0:
+                raise ValueError
+        except ValueError:
+            return abort(400)
+        page = laureats[p * 50: (p + 1) * 50]
+        return jsonify(
+            {
+                "page": p,
+                "count_on_page": PAGE_SIZE,
+                "total": len(laureats),
+                "items": page,
+            }
+        )
+
+
 # TODO: Добавить код для получения лауреата по индексу
+
+
+@ns.route("/laureat/<int:pk>/")
+class Laureat(Resource):
+    def get(self, pk):
+        if 0 <= pk < len(laureats):
+            return jsonify(laureats[pk])
+        else:
+            abort(404)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
